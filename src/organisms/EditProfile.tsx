b2c5a8765c3beb30da8df9ffc8ac8a9939fd2fa1
Abas Muzi <106030034+AbasPortfolio@users.noreply.github.com>
2025-12-10 00:24:24 +0200

    Update EditProfile.tsx

--- src/organisms/EditProfile.tsx
+++ src/organisms/EditProfile.tsx
@@ -26,7 +26,7 @@ function EditProfile() {
             try {
                 const userId = localStorage.getItem("userId");
                 if (userId) {
-                    const response = await axios.patch(`http://localhost:5050/record/user/update/${userId}`, {
+                    const response = await axios.patch(`https://backendrecords.onrender.com/record/user/update/${userId}`, {
                         email: email,
                         username: name,
                         doctor: isDoctor
